13afc673568c205b6fc422f41966d576f65d0863
dognmrt <46164888+dognmrt@users.noreply.github.com>
2021-01-19 01:53:39 +0300

    dataset splitted to train and test parts

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
-#Murat Dogan 170709033 Data Mining
+from sklearn.model_selection import train_test_split
 
 #reads dataset
 df = pd.read_csv("dataset.csv")
-#prints dataset
-print(df)
+labels = df.label
+
+#split data test and train part
+X_train, X_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.2, random_state=7)
