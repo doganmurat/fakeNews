457cae4a17c939ee877d06c794042e1ade9999dd
dognmrt <46164888+dognmrt@users.noreply.github.com>
2021-01-21 22:02:12 +0300

    vectorizer implemented

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-
-#reads dataset
-df = pd.read_csv("dataset.csv")
-labels = df.label
-
-#split data test and train part
-X_train, X_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+#dataset
+dataset = pd.read_csv("dataset.csv")
+labels = dataset.label
+#splitting train and test part
+X_train, X_test, y_train, y_test = train_test_split(dataset['text'], labels,
+                                                    test_size=0.3, random_state=10)
+#vectorizer
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_train = tfidf.fit_transform(X_train)
+tfidf_test = tfidf.transform(X_test)
